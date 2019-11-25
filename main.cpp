b5dfc974b79bdf687615aa032dfc9807a37a02c3
maui <56946443+mauiaquino@users.noreply.github.com>
2019-11-25 12:11:42 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+int ArraySize();
+void inputArray(int new_age[]);
+void displayArray(int new_age[]);
+
+int arrSize;
+
+
+
+int main();
+{
+
+    int new_age[arrSize];
+    void inputArray(int new_age[]);
+    void displayArray(int new_age[]);
+
+    return arrSize;
+}
+
+int ArraySize()
+{
+
+    int new_age[arrSize];
+    cout << "enter value for position"<< endl;
+    cin>> new_age[arrSize];
+
+    return arrSize;
+}
+
+    void inputArray(){
+    int new_age[arrSize];
+    for(int i=0; i<=arrSize-1;i++){
+        cout<<"enter Value for position"<< i +1<< " : ";
+        cin>>new_age[arrSize];
+}
+    }
+
+
+
+    void displayArray(){
+    int new_age[arrSize];
+    for(int i=0; i<=arrSize-1;i++){
+    cout<<"enter Value for position"<< i +1<<"|"<<endl;
+    cin>> new_age[arrSize];
+    }
+    }
+
